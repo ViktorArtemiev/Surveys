79cadd4b0c49aa2e7d015651227cadd910bb16e6
Viktor Artemiev <vik.artemiev@gmail.com>
2019-07-28 19:27:30 +0300

    chore: cover Survey data provider by Unit tests

---
+++ app/src/test/java/co/nimblehq/data/survey/SurveyRepositoryTest.kt
@@ -0,0 +1,95 @@
+package co.nimblehq.data.survey
+
+import co.nimblehq.data.model.Survey
+import co.nimblehq.data.source.survey.SurveyRepository
+import co.nimblehq.data.source.survey.SurveyService
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mockito
+import retrofit2.Response
+import java.io.IOException
+
+
+/**
+ * Created by Viktor Artemiev on 2019-07-28.
+ * Copyright (c) 2019, Nimble. All rights reserved.
+ */
+@RunWith(JUnit4::class)
+class SurveyRepositoryTest {
+
+    lateinit var service: SurveyService
+    lateinit var surveys: List<Survey>
+
+    @Before
+    fun setup() {
+        service = Mockito.mock(SurveyService::class.java)
+        surveys = mutableListOf(
+            Survey(
+                id = "1",
+                title = "Oishi Buffet",
+                description = "Customer Experience Survey",
+                image = "https://dhdbhh0jsld0o.cloudfront.net/m/1"
+            ),
+            Survey(
+                id = "2",
+                title = "Segafredo",
+                description = "Customer Satisfaction Survey",
+                image = "https://dhdbhh0jsld0o.cloudfront.net/m/2"
+            ),
+            Survey(
+                id = "3",
+                title = "Tops Super Store",
+                description = "Customer Survey",
+                image = "https://dhdbhh0jsld0o.cloudfront.net/m/3"
+            ))
+    }
+
+    @Test
+    fun `when get surveys - calls the correct API method`() {
+        val repository = SurveyRepository(service)
+        Mockito.`when`(service.getSurveys(page = 1, perPage = 3))
+            .thenReturn(CompletableDeferred(Response.success(surveys)))
+        runBlocking {
+            repository.getSurveys(page = 1, pageSize = 3)
+            Mockito.verify(service).getSurveys(page = 1, perPage = 3)
+        }
+    }
+
+    @Test
+    fun `get surveys and succeed`() {
+        val repository = SurveyRepository(service)
+        Mockito.`when`(service.getSurveys(page = 1, perPage = 3))
+            .thenReturn(CompletableDeferred(Response.success(surveys)))
+        runBlocking {
+            val surveys = repository.getSurveys(page = 1, pageSize = 3)
+            assertNotNull(surveys)
+            assertFalse(surveys.isEmpty())
+            assertEquals(3, surveys.size)
+            assertEquals("1", surveys.first().id)
+            assertEquals("Oishi Buffet", surveys.first().title)
+            assertEquals("Customer Experience Survey", surveys.first().description)
+            assertEquals("https://dhdbhh0jsld0o.cloudfront.net/m/1", surveys.first().image)
+        }
+    }
+
+    @Test(expected = IOException::class)
+    fun `refresh token and fail`() {
+        val repository = SurveyRepository(service)
+        Mockito.`when`(service.getSurveys(page = 1, perPage = 3))
+            .thenReturn(
+                CompletableDeferred(
+                    Response.error(401, ResponseBody.create(
+                        MediaType.parse("application/json"), byteArrayOf())))
+            )
+        runBlocking {
+            repository.getSurveys(page = 1, pageSize = 3)
+        }
+    }
+}
---
+++ app/src/test/java/co/nimblehq/data/survey/SurveyServiceTest.kt
@@ -0,0 +1,57 @@
+package co.nimblehq.data.survey
+
+import co.nimblehq.data.BaseServiceTest
+import co.nimblehq.data.model.Survey
+import co.nimblehq.data.source.survey.SurveyService
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.core.Is
+import org.hamcrest.core.IsNull
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+/**
+ * Created by Viktor Artemiev on 2019-07-28.
+ * Copyright (c) 2019, Nimble. All rights reserved.
+ */
+@RunWith(JUnit4::class)
+class SurveyServiceTest : BaseServiceTest() {
+
+    private lateinit var service: SurveyService
+
+    @Before
+    override fun setup() {
+        super.setup()
+        service = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+            .create(SurveyService::class.java)
+    }
+
+    @Test
+    fun `get surveys`() {
+        mockResponse("surveys.json")
+        runBlocking {
+            val response: Response<List<Survey>> = service.getSurveys(page = 1, perPage = 10).await()
+            val surveys = response.body()
+            assertThat(surveys, IsNull.notNullValue())
+            assertFalse(surveys!!.isEmpty())
+            assertTrue(surveys.size == 10)
+            val survey = surveys.first()
+            assertThat(survey.id, Is.`is`("df5c6cad88e3865aede0"))
+            assertThat(survey.title, Is.`is`("Oishi Buffet"))
+            assertThat(survey.description, Is.`is`("Customer Experience Survey"))
+            assertThat(survey.image, Is.`is`("https://dhdbhh0jsld0o.cloudfront.net/m/6b663dd6fb7a200ad4c7_"))
+        }
+    }
+
+}
