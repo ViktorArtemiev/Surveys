521c331b4ca4933b35be1ee4ac70c8735a8c18e0
Viktor Artemiev <vik.artemiev@gmail.com>
2019-08-02 06:30:08 +0300

    implement TokenAuthenticator

---
+++ app/src/main/java/co/nimblehq/authenticator/TokenAuthenticator.kt
@@ -0,0 +1,39 @@
+package co.nimblehq.authenticator
+
+import co.nimblehq.SurveysAccount
+import co.nimblehq.data.source.token.TokenRepository
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+
+
+/**
+ * Created by Viktor Artemiev on 2019-08-01.
+ * Copyright (c) 2019, Nimble. All rights reserved.
+ */
+class TokenAuthenticator(
+    private val account: SurveysAccount,
+    private val tokenRepository: TokenRepository
+) : Authenticator {
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        return if (response.code() == 401) { //if unauthorized
+            refreshToken()
+            response.request()
+                .newBuilder()
+                .header("Authorization", account.provideToken())
+                .build()
+        } else {
+            null
+        }
+    }
+
+    private fun refreshToken() {
+        runBlocking {
+            val token = tokenRepository.refreshToken(account.provideUser())
+            account.refreshToken(token)
+        }
+    }
+}
